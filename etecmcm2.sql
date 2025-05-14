4963e66a1850f69c4a1e31b6b996f4418e5cecbc
Vinicius Luz dos Santos <159595979+wViniciusLuzw@users.noreply.github.com>
2025-05-14 12:45:00 -0300

    Add files via upload

--- etecmcm2.sql
+++ etecmcm2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Maio-2025 às 17:38
+-- Tempo de geração: 14-Maio-2025 às 17:44
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.1.2
 
@@ -62,6 +62,37 @@ CREATE TABLE `produtos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_produto`, `nome`, `preco`, `quant`, `marca`, `id_categoria`) VALUES
+(1, 'Sabão em pó', '30.50', 50, 'OMO', 1),
+(2, 'Detergente', '3.55', 10, 'Ypê', 1),
+(3, 'Esponja multiuso', '14.09', 10, 'Scotch Brite', 1),
+(4, 'Água sanitária', '11.39', 1, 'Ypê', 1),
+(5, 'Limpador Sanitário Gel', '17.99', 1, 'Pato', 1),
+(6, 'Sabonete Líquido', '39.90', 3, 'Protex', 2),
+(7, 'Shampoo', '44.00', 1, 'Kérastase', 2),
+(8, 'Condicionador', '28.99', 1, 'TRESemmé', 2),
+(9, 'Lâmina de barbear', '42.59', 8, 'Gillette', 2),
+(10, 'Creme Depilatório', '37.69', 3, 'Veet', 2),
+(11, 'Manteiga', '21.49', 1, 'Tirol', 3),
+(12, 'Leite', '79.90', 12, 'Tirol', 3),
+(13, 'Queijo Mussarela', '29.99', 1, 'Ipanema', 3),
+(14, 'Iogurte Natural', '3.79', 1, 'Nestlé', 3),
+(15, 'Iogurte Morango', '3.49', 1, 'Danone', 3),
+(16, 'Picanha Nobre', '109.98', 1, 'Swift', 4),
+(17, 'Linguiça Toscana', '14.95', 1, 'Sadia', 4),
+(18, 'Peito de Frango', '13.99', 1, 'Lar', 4),
+(19, 'Cupim Bovino', '39.90', 1, 'Swift', 4),
+(20, 'Barriga Suína', '11.25', 1, 'Coop', 4),
+(21, 'Ameixa em Calda', '7.69', 1, 'Oderich', 5),
+(22, 'Feijoada Lata', '10.69', 1, 'Oderich', 5),
+(23, 'Carne Bovina em Conserva', '12.95', 1, 'Anglo', 5),
+(24, 'Milho ao Vapor', '14.50', 1, 'Bonduelle', 5),
+(25, 'Atum em Pedaços', '10.79', 1, 'Gomes da Costa', 5);
+
+--
 -- Índices para tabelas despejadas
 --
 
@@ -92,7 +123,7 @@ ALTER TABLE `categorias`
 -- AUTO_INCREMENT de tabela `produtos`
 --
 ALTER TABLE `produtos`
-  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- Restrições para despejos de tabelas
